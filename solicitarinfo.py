07fc2850a40d8cab4dcd80f6f47451bb63b32e2d
christobalp <cr.pizarrom@duocuc.cl>
2024-04-12 16:14:31 +0000

    Committing datosjson.py to begin tracking changes

---
+++ solicitarinfo.py
@@ -0,0 +1,6 @@
+Nombre = input("EN primer lugar porfavor ingresar su nombre: ")
+Apellido = input("a continuación ingrese su apellido: ")
+CodigoSeccion = input("Ingrese el codigo sección de su asignatura: ")
+Sede = input("INgrese la sede a la cual pertenece: ")
+
+print("Ha sido identificado como estudiante de DUOC, usted se llama", Nombre, Apellido, ", tiene un codigo de sec", CodigoSeccion, "pertenece a la sede de ", Sede)
